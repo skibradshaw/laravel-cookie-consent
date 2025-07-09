5e0f2a1bfcda44ac448113143b017dcc3fdcec62
Tim Bradshaw <tim@alltrips.com>
2025-07-09 11:36:34 -0600

    Modify Routes to work with Laravel 7

--- routes/laravel-cookie-consent.php
+++ routes/laravel-cookie-consent.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Devrabiul\CookieConsent\Http\Controllers\CookieConsentController;
 
-Route::controller(CookieConsentController::class)->group(function () {
-    Route::get('/laravel-cookie-consent/script-utils', 'scriptUtils')->name('laravel-cookie-consent.script-utils');
-});
+Route::get('/laravel-cookie-consent/script-utils', [CookieConsentController::class, 'scriptUtils'])->name('laravel-cookie-consent.script-utils');
+
 
